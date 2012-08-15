73f25b3ea89710b14e32e80eaba362ea117d6cea
Timothy Edmund Crosley <timothy.crosley@gmail.com>
2012-08-15 16:31:35 -0400

    Set setup license to GPLv2

--- setup.py
+++ setup.py
@@ -9,4 +9,5 @@ setup(name='WebElements',
       author_email='timothy.crosley@gmail.com',
       url='http://www.webelements.in/',
       download_url='https://github.com/timothycrosley/webelements/blob/master/dist/WebElements-0.1.tar.gz?raw=true',
+      license = "GNU GPLv2",
       packages=['WebElements',],)
